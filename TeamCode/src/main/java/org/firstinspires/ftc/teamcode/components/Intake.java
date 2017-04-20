c5989fcfb7922da19f8bbc21a5e2916169104769
benorg <benorg@me.com>
2017-04-19 22:20:06 -0400

    removed unused imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Intake.java
@@ -3,12 +3,9 @@ package org.firstinspires.ftc.teamcode.components;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.teamcode.utilities.DelayedAction;
 import org.firstinspires.ftc.teamcode.utilities.Hardware;
-import org.firstinspires.ftc.teamcode.utilities.Utils;
 
 /**
  * Created by benorgera on 11/24/16.
